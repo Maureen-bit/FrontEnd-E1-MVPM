102157462a1f942d09c22f163d2be6d53d395112
Maureen Parra Mondragon <maureenparramondragon@Maureens-MacBook-Air.local>
2022-08-25 19:15:06 -0500

    test first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+
 function App() {
 
   return (
