1bcdf9077b9e3b6896fff4b49ffd9a0cc3b951ea
Maureen Parra Mondragon <maureenparramondragon@Maureens-MacBook-Air.local>
2022-09-01 18:41:08 -0500

    app finalizada

--- src/components/Item.js
+++ src/components/Item.js
@@ -9,11 +9,30 @@
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
 
-export default function Item() {
+import { useState } from "react";
+
+export default function Item({producto, stock, setTotalProductosComprados, totalProductosComprados}) {
+  const { nombre, descripcion, url } = producto;
+  const [ itemStock, setItemStock ] = useState(stock);
+
+  const modificarStocks = () => {
+    setTotalProductosComprados(totalProductosComprados + 1)
+    setItemStock(itemStock - 1)
+  }
+
+  const renovarStocks = () => {
+    setTotalProductosComprados(totalProductosComprados - 1)
+    setItemStock(itemStock + 1)
+  }
 
   return (
     <div className='producto'>
-      {/* maquetar Item aquí */}
+      <h3>{nombre}</h3>
+      <p>{descripcion}</p>
+      <img src={url}/>
+      <h5>En stock: <span className={itemStock <= 0 ? 'noStock' : 'stock'} >{itemStock <= 0 ? 'Agotado' : itemStock}</span></h5>
+      <button disabled={itemStock <= 0 ? true : false} onClick={modificarStocks}>Agregar al carrito</button>
+      <button disabled={itemStock === stock ? true : false} onClick={renovarStocks}>Eliminar del carrito</button>
     </div>
   )
 }
